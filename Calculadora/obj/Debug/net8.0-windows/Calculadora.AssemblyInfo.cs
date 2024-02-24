774709a2ffaecf5ce188ecbefcb8ef62bc25bb2e
mishFi20 <dsljdlls>
2024-02-23 19:22:02 -0600

    Agrego readme

--- Calculadora/obj/Debug/net8.0-windows/Calculadora.AssemblyInfo.cs
+++ Calculadora/obj/Debug/net8.0-windows/Calculadora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa21bc3fbc2245bf6666a67d85ad63cea8616fe2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bdc9196659589d894a6a954c332ba1444cc9187")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
